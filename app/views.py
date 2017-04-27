4164c6609a393190bd67542777893df80919c490
wex <3060106630@qq.com>
2017-04-27 15:52:24 +0800

    '3'

--- app/views.py
+++ app/views.py
@@ -82,7 +82,7 @@ def share(request):
 		form = ShareForm(user=request.user.profile)
 
                     
-	return render(request, 'app\share.html',
+	return render(request, 'app/share.html',
 					{'form': form} )
 
 @login_required
