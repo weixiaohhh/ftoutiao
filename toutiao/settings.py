bc22019d689028f1c4522ca1f806d510ead2a17b
wex <3060106630@qq.com>
2017-04-27 10:47:32 +0800

    '3'

--- toutiao/settings.py
+++ toutiao/settings.py
@@ -92,9 +92,9 @@ WSGI_APPLICATION = 'toutiao.wsgi.application'
 
 import dj_database_url
 try:
-    from .local_setting import *
+	from .local_setting import *
 except Exception as e:
-    SECRET_KEY = config('SECRET_KEY')
+	SECRET_KEY = config('SECRET_KEY')
 	DEBUG = config('DEBUG', default=False, cast=bool)
 	DATABASES = {
     'default': dj_database_url.config(
