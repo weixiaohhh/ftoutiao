533a545efbfca72c6d7acb2436243b80eb418d9f
weixiaohhh <3060106630@qq.com>
2017-05-31 14:17:05 +0800

    Create local_settings.py

---
+++ toutiao/local_settings.py
@@ -0,0 +1,15 @@
+
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['localhost']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
